3a6af1d312734729f7d793bd94d6be2cf350d948
jaacobgomez <jacobsen0105@gmail.com>
2026-01-07 19:44:23 +0100

    Mover .env.local

--- .next/dev/server/middleware-build-manifest.js
+++ .next/dev/server/middleware-build-manifest.js
@@ -1,31 +1,6 @@
 globalThis.__BUILD_MANIFEST = {
   "pages": {
-    "/_app": [
-      "static/chunks/0ce26_next_dist_compiled_fe1a6ad7._.js",
-      "static/chunks/0ce26_next_dist_shared_lib_89c52726._.js",
-      "static/chunks/0ce26_next_dist_client_0efaf27b._.js",
-      "static/chunks/0ce26_next_dist_4b6554c7._.js",
-      "static/chunks/0ce26_next_app_cc881a8f.js",
-      "static/chunks/[next]_entry_page-loader_ts_88824a3b._.js",
-      "static/chunks/0ce26_react-dom_3b1bbb23._.js",
-      "static/chunks/0ce26_cc5f978c._.js",
-      "static/chunks/[root-of-the-server]__4e220456._.js",
-      "static/chunks/Practica_Final_spotify2_Final_web_pages__app_2da965e7._.js",
-      "static/chunks/turbopack-Practica_Final_spotify2_Final_web_pages__app_3299dac6._.js"
-    ],
-    "/_error": [
-      "static/chunks/0ce26_next_dist_compiled_fe1a6ad7._.js",
-      "static/chunks/0ce26_next_dist_shared_lib_7c320d68._.js",
-      "static/chunks/0ce26_next_dist_client_0efaf27b._.js",
-      "static/chunks/0ce26_next_dist_4564647b._.js",
-      "static/chunks/0ce26_next_error_38481609.js",
-      "static/chunks/[next]_entry_page-loader_ts_4a9f81d3._.js",
-      "static/chunks/0ce26_react-dom_3b1bbb23._.js",
-      "static/chunks/0ce26_cc5f978c._.js",
-      "static/chunks/[root-of-the-server]__90d92469._.js",
-      "static/chunks/Practica_Final_spotify2_Final_web_pages__error_2da965e7._.js",
-      "static/chunks/turbopack-Practica_Final_spotify2_Final_web_pages__error_7086ab59._.js"
-    ]
+    "/_app": []
   },
   "devFiles": [],
   "polyfillFiles": [
